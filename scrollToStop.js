5ce85715d84593417101375530bb3a045b8d5045
Wylie <wylie@dukeofcheese.com>
2012-04-19 00:12:16 -0400

    Still cleaning things up…

--- scrollToStop.js
+++ scrollToStop.js
@@ -1,28 +1,17 @@
-/* ********************
-
-* 4/17/2012 Wylie Fisher
-* dukeofcheese.com
-
-******************** */
-
+/* ***************************************** */
+/* 4/17/2012 Wylie Fisher - dukeofcheese.com */
+/* ***************************************** */
 (function( $ ){
     jQuery.fn.scrollToStop = function(options){
-
-        // set the default for the height if no user input
         if ( options == undefined ) {
 			var options = 0;
         };
-               
         var elem = $(this);
             elemPos = $(elem).position();    
             elemStyle = $(elem).css('position');
-            
-        /* check for position:absolute on the element. if not found, add it. */
         if( elemStyle != 'absolute' ) {
             $(elem).css({'position': 'absolute', 'top': elemPos.top});
         }
-        
-        /* on window scroll watch the poisition of the element. */
         $(window).on('scroll', function() {
             var scrollTop = $(window).scrollTop();
                 elemPosFixed = elemPos.top - scrollTop;
